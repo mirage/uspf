2b3b98a5150b5868cf3e657e6e37b1f03c4c8380
Calascibetta Romain <romain.calascibetta@gmail.com>
2024-10-10 16:57:40 +0200

    Add a mirage derivation of this library

---
+++ lib-mirage/uspf_mirage.mli
@@ -0,0 +1,12 @@
+module Make (Dns_client : Dns_client_mirage.S) : sig
+  val get :
+       ctx:Uspf.ctx
+    -> Dns_client.t
+    -> ([ Uspf.res | `Record of Uspf.record ], [> `Msg of string ]) result Lwt.t
+
+  val check :
+       ctx:Uspf.ctx
+    -> Dns_client.t
+    -> [ Uspf.res | `Record of Uspf.record ]
+    -> Uspf.res Lwt.t
+end
